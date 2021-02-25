80ad1572c32d82b8688400741772687bcac6db5f
Netto Fernandes <you@example.com>
2021-02-25 11:43:11 -0300

    rework

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
 ? 'http://localhost:8081'
-: 'https://javaflix.herokuapp.com';
+: 'https://javaflixx.herokuapp.com';
 
 export default {
     URL_BACKEND,
